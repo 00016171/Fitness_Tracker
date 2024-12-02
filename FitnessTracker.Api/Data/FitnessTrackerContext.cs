d72f8bd6adbb749536f46e8c553fc2068c7293c5
JasurKhushbokov <khushbakovjasur@gmail.com>
2024-12-02 11:34:33 -0700

    Initial commit

---
+++ FitnessTracker.Api/Data/FitnessTrackerContext.cs
@@ -0,0 +1,24 @@
+﻿using FitnessTracker.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessTracker.Api.Data
+{
+    public class FitnessTrackerContext : DbContext
+    {
+        public FitnessTrackerContext(DbContextOptions<FitnessTrackerContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure relationships
+            modelBuilder.Entity<Activity>()
+                .HasOne(a => a.User)
+                .WithMany(u => u.Activities)
+                .HasForeignKey(a => a.UserId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
